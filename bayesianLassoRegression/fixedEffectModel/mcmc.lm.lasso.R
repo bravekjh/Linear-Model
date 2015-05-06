24e3413d77e0d02206566e67e2fca13b8e30b2db
jtipton25 <jtipton25@gmail.com>
2015-05-06 16:43:17 -0600

    end of day update

--- bayesianLassoRegression/fixedEffectModel/mcmc.lm.lasso.R
+++ bayesianLassoRegression/fixedEffectModel/mcmc.lm.lasso.R
@@ -5,99 +5,138 @@
 ##
 
 ##
-## model: Y = X %*% beta + epsilon
+## model: y = X %*% beta + epsilon
 ##
 
-##
-## libraries and functions
-##
-
-mcmc.lm.lasso <- function(Y, X, n.mcmc, alpha.epsilon, beta.epsilon, alpha.lambda, beta.lambda){
-
-##
-## Initialize variables
-##
-
-n <- length(Y)
-tau <- dim(X)[2]
-# sigma.squared.epsilon
-sigma.squared.epsilon <- 1 / rgamma(1, alpha.epsilon, beta.epsilon) 
-# lambda
-lambda.squared <- rgamma(1, alpha.lambda, beta.lambda)
-# gamma
-gamma.squared <- rgamma(tau, 1, lambda.squared / 2)
-Dgamma <- diag(gamma.squared)
-# beta
-mu.beta <- rep(0, tau)
-beta <- rMVN(chol(sigma.squared.epsilon * Dgamma), mu.beta)
-
-##
-## save variables
-##
-
-beta.save <- matrix(nrow = tau, ncol = n.mcmc)
-sigma.squared.epsilon.save <- vector(length = n.mcmc)
-gamma.squared.save <-  matrix(nrow = tau, ncol = n.mcmc)
-lambda.squared.save <- vector(length = n.mcmc)
-Dbar.save <- vector(length = n.mcmc)
-
-##
-## Start MCMC
-##
-
-for(k in 1:n.mcmc){
-  if(k %% 100 == 0){
-    cat(" ", k)
+mcmc <- function(y, X, H, n_mcmc, alpha_epsilon, beta_epsilon, alpha_lambda, beta_lambda, pca = FALSE){
+  
+  ##
+  ## libraries and functions
+  ##
+  
+  library(myFunctions)
+  
+  ##
+  ## Initialize variables
+  ##
+  
+  n <- length(y)
+  tau <- dim(X)[2]
+  if(pca == TRUE){
+    X_pca = makePCA(X)
+    Lambda_inv = diag(1/ as.vector(X_pca$sdev^2))
+    X = X_pca$X_pca 
+  } else {
+    Lambda_inv = diag(tau)
   }
-
-  ##
-  ## sample beta
-  ##
   
-  A.chol <- chol(t(X) %*% X + Dgamma) ## check if I need variance term here
-  b <- (t(X) %*% Y) ## check if I need variance term here
-  beta <- rMVN(A.chol, b)
+  # s2_epsilon
+  s2_epsilon <- max(10, 1 / rgamma(1, alpha_epsilon, beta_epsilon))
+  sqrt_s2_epsilon = sqrt(s2_epsilon)
+  # lambda
+  lambda2 <- rgamma(1, alpha_lambda, beta_lambda)
+  # gamma
+  gamma2_inv <- 1 / rgamma(tau, 1, lambda2 / 2)
+  D_gamma_inv <- diag(gamma2_inv)
+  D_gamma_Lambda_inv = D_gamma_inv %*% Lambda_inv
+  # beta
+  ybar = mean(y)
+  J_mu = rep(1, length(y))
+  mu <- J_mu * ybar
+  y_minus_mu = y - mu
+  mu_beta <- rep(0, tau)
+  beta <- as.vector(mvrnormArma(1, mu_beta, s2_epsilon * solve(D_gamma_inv)))
   
-  ##
-  ## sample sigma.squared.epsilon
-  ##
-  
-  sigma.squared.epsilon <- 1 / rgamma(1, alpha.epsilon + n / 2 + tau / 2, beta.epsilon + 1 / 2 * t(Y - X %*% beta) %*% (Y - X %*% beta) + 1 / 2 * t(beta) %*% Dgamma %*% beta)
-      
-  ##
-  ## sample gamma.squared
-  ##
-  
-  mu.tilde <- sqrt(lambda.squared * sigma.squared.epsilon / beta^2)
-  lambda.tilde <- lambda.squared
-  gamma.squared <- rinvgauss(tau, mu.tilde, lambda.tilde)
-  Dgamma <- diag(as.vector(gamma.squared))
-  
-  ##
-  ## lambda.squared
-  ##
-  
-  lambda.squared <- rgamma(1, alpha.lambda + tau, beta.lambda + sum(gamma.squared) / 2)
-  
-  ##
-  ## DIC calculations
-  ##
-  
-  Dbar.save[k] <- - 2 * sum(dnorm(Y, X %*% beta, sqrt(sigma.squared.epsilon), log = TRUE))
+  ## variables for speed
+  HX = X[H, ]
+  tHX = t(X[H, ])
+  tHXX = tHX %*% X[H, ]
+  HXbeta = HX %*% beta
+  sqrt_n = sqrt(n)
   
   ##
   ## save variables
   ##
   
-  beta.save[, k] <- beta
-  sigma.squared.epsilon.save[k] <- sigma.squared.epsilon
-  gamma.squared.save[, k] <- gamma.squared
-  lambda.squared.save[k] <- lambda.squared
+  n_burn=n_mcmc / 2
+  n_save = n_mcmc - n_burn
+  mu_save = rep(0, n_save)
+  beta_save <- matrix(nrow = tau, ncol = n_save)
+  s2_epsilon_save <- vector(length = n_save)
+  gamma2_save <-  matrix(nrow = tau, ncol = n_save)
+  lambda2_save <- vector(length = n_save)
+  Dbar_save <- vector(length = n_save)
+  
+  ##
+  ## Start MCMC
+  ##
+  
+  for(k in 1:n_mcmc){
+    if(k %% 100 == 0){
+      cat(" ", k)
+    }
+    
+    ##
+    ## sample mu
+    ##
+    
+    a = 1 / s2_epsilon * n
+    b = 1 / s2_epsilon * sum(y - HXbeta)
+    mu = rMVNArmaScalar(a, b)
+    y_minus_mu = y - mu
+    
+    ##
+    ## sample beta
+    ##
+    
+    A <- 1 / s2_epsilon * (tHXX + D_gamma_Lambda_inv) ## check if I need variance term here
+    b <- 1 / s2_epsilon * (tHX %*% y_minus_mu) ## check if I need variance term here
+    beta <- rMVNArma(A, b)
+    HXbeta = HX %*% beta
+    
+    ##
+    ## sample s2_epsilon
+    ##
+    
+    deviance = y_minus_mu - HXbeta
+    s2_epsilon <- 1 / rgamma(1, alpha_epsilon + n / 2 + tau / 2, beta_epsilon + 1 / 2 * t(deviance) %*% deviance + 1 / 2 * t(beta) %*% D_gamma_Lambda_inv %*% beta)
+    sqrt_s2_epsilon = sqrt(s2_epsilon)
+    
+    ##
+    ## sample gamma2
+    ##
+    
+    mu_tilde <- sqrt(lambda2 * s2_epsilon / beta^2)
+    lambda_tilde <- lambda2
+    gamma2_inv <- rinvgauss(tau, mu_tilde, lambda_tilde)
+    D_gamma_inv <- diag(as.vector(gamma2_inv))
+    D_gamma_Lambda_inv = D_gamma_inv %*% Lambda_inv
+    gamma2 <- 1 / gamma2_inv
+    
+    ##
+    ## lambda2
+    ##
+    
+    lambda2 <- rgamma(1, alpha_lambda + tau, beta_lambda + sum(gamma2) / 2)
+    
+    ##
+    ## save variables
+    ##
+    if(k > n_burn){
+      k_tmp = k - n_burn
+      ## DIC calculations
+      Dbar_save[k_tmp] <- - 2 * sum(dnorm(y, mu + HXbeta, sqrt_s2_epsilon, log = TRUE))
+      mu_save[k_tmp] <- mu[1]
+      beta_save[, k_tmp] <- beta
+      s2_epsilon_save[k_tmp] <- s2_epsilon
+      gamma2_save[, k_tmp] <- 1 / gamma2_inv
+      lambda2_save[k_tmp] <- lambda2
+    }
   }
-
-##
-## output
-##
-
-list(beta.save = beta.save, sigma.squared.epsilon.save = sigma.squared.epsilon.save, gamma.squared.save = gamma.squared.save, lambda.squared.save = lambda.squared.save)
+  
+  ##
+  ## output
+  ##
+  
+  list(mu = mu_save, beta = beta_save, s2_epsilon = s2_epsilon_save, gamma2 = gamma2_save, lambda2 = lambda2_save, X = X)
 }
--- bayesianLassoRegression/fixedEffectModel/script.lm.lasso.R
+++ bayesianLassoRegression/fixedEffectModel/script.lm.lasso.R
@@ -12,17 +12,14 @@ setwd("~/Linear-Model/bayesianLassoRegression/fixedEffectModel/")
 source("mcmc.lm.lasso.R")
 
 make.model.plot <- function(out){
-  n.burn <- floor(n.mcmc / 5) + 1
-  layout(matrix(1:9, 3))
-  matplot(t(out$beta.save[, n.burn:n.mcmc]), type = 'l')
-  #hist(out$beta.save[1,][n.burn:n.mcmc])
-  #abline(v = beta[1], col = 'red')
-  hist(out$beta.save[2,][n.burn:n.mcmc], main = 'Posterior of Beta2')
+  layout(matrix(1:6, 3, 2))
+  matplot(out$mu, type = 'l')
+  matplot(t(out$beta), type = 'l')
+  hist(out$beta[2,], main = 'Posterior of Beta2')
   abline(v = beta[2], col = 'red')
-  plot(out$sigma.squared.epsilon.save[n.burn:n.mcmc], type = 'l')
-  abline(h = sigma.squared.epsilon, col = 'red')
-  plot(out$lambda.squared.save, type = 'l')
-#  matplot(t(out$gamma.squared.save[, n.burn:n.mcmc]),  type = 'l')
+  plot(out$s2_epsilon, type = 'l')
+  abline(h = s2_epsilon, col = 'red')
+  plot(out$lambda2, type = 'l')
 }
 
 
@@ -33,10 +30,10 @@ make.model.plot <- function(out){
 N <- 1000
 n <- 100
 beta <- -3:3
-sigma.squared.epsilon <- 0.25
+s2_epsilon <- 0.25
 tau <- length(beta)
 
-make.lm.data <- function(N, n, beta, sigma.sqaured.epsilon){
+make.lm.data <- function(N, n, beta, s.sqaured_epsilon){
   tau <- length(beta)
   X <- matrix(nrow = N, ncol = tau)
   for(i in 1:tau){
@@ -44,15 +41,15 @@ make.lm.data <- function(N, n, beta, sigma.sqaured.epsilon){
   }
 #   X <-matrix(c(rep(1, N), rep(seq(0, 1, length = N), tau - 1)), nrow = N, ncol = tau)
 #   if(is.null(dim(X))){
-#     Y <- X * beta + rnorm(N, 0, sigma.squared.epsilon)
+#     Y <- X * beta + rnorm(N, 0, s2_epsilon)
 #   } else {
-    Y <- X %*% beta + rnorm(N, 0, sigma.squared.epsilon)
+    Y <- X %*% beta + rnorm(N, 0, s2_epsilon)
 #   }
-  #list(X = X, Y = Y, N = N, n = n, sigma.squared.epsilon = sigma.squared.epsilon)
+  #list(X = X, Y = Y, N = N, n = n, s2_epsilon = s2_epsilon)
   data.frame(Y, X)
 }
 
-data <- make.lm.data(N, n, beta, sigma.squared.epsilon)
+data <- make.lm.data(N, n, beta, s2_epsilon)
 
 samp <- sample(1:N, n)
 data.samp <- data[samp, ]
@@ -63,24 +60,24 @@ lm(Y ~ . ,data = data)
 ## Setup priors
 ##
 
-# hyperparameters for mu.beta and sigma.squared.beta
-alpha.epsilon <- 10
-beta.epsilon <- 10
-alpha.lambda <- 10
-beta.lambda <- 10
+# hyperparameters for mu.beta and s2.beta
+alpha_epsilon <- 0.1
+beta_epsilon <- 0.1
+alpha_lambda <- 0.1
+beta_lambda <- 0.1
 # 
 # mu.0 <- rep(0, tau)
-# sigma.squared.0 <- 100 
-# # hyerparamters for sigma.squared.beta
+# s2.0 <- 100 
+# # hyerparamters for s2.beta
 # alpha.beta <- 2
 # beta.beta <- 10
 # curve(dinvgamma(x, alpha.beta, beta.beta), from = 0, to = 10)
-# # hyperparameters for sigma.squared.epsilon
-# alpha.epsilon <- 2
-# beta.epsilon <- 10
-# curve(dinvgamma(x, alpha.epsilon, beta.epsilon), from = 0, to = 10)
+# # hyperparameters for s2_epsilon
+# alpha_epsilon <- 2
+# beta_epsilon <- 10
+# curve(dinvgamma(x, alpha_epsilon, beta_epsilon), from = 0, to = 10)
 
-n.mcmc <- 5000
+n_mcmc <- 5000
 
 ##
 ## Fit mcmc
@@ -89,7 +86,7 @@ n.mcmc <- 5000
 Y <- data.samp[, 1]
 X <- as.matrix(data.samp[, 2:(tau + 1)], ncol = tau)
 
-out <- mcmc.lm.lasso(Y, X, n.mcmc, alpha.epsilon, beta.epsilon, alpha.lambda, beta.lambda)
+out <- mcmc(Y, X, n_mcmc, alpha_epsilon, beta_epsilon, alpha_lambda, beta_lambda)
               
 
 make.model.plot(out)
